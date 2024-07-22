951271fd3b803b09da5ccc3d64a6eb19a27c17bd
jimmy2414 <juanignaciomunozok@gmail.com>
2024-07-22 20:41:23 -0300

    sss

--- api/server.js
+++ api/server.js
@@ -8,12 +8,12 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-    "/blog/:resource/:id/show": "/:resource/:id",
-  })
-);
+// server.use(
+//   jsonServer.rewriter({
+//     "/api/*": "/$1",
+//     "/blog/:resource/:id/show": "/:resource/:id",
+//   })
+// );
 server.use(router);
 server.listen(3000, () => {
   console.log("JSON Server is running");
